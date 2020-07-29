26f74196b80968fe168fbb713e26395495be3072
arpitpardesi <arpit.pardesi6@gmail.com>
2020-07-29 11:20:09 +0530

    NumPy

--- Basics/Day4/NumPy/Ex2.py
+++ Basics/Day4/NumPy/Ex2.py
@@ -2,6 +2,10 @@ import numpy as np
 
 a = np.array([1,2,3,4,5])
 print(a.itemsize)
-
+print(a.dtype)
+print(a.size)
+print("-----------------------")
 a2 = np.array([(1,2,3),(4,5,6),(7,8,9)])
 print(a2.itemsize)
+print(a2.dtype)
+print(a2.size)
