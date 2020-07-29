f903b90422f4cc1a12ce49f2e062fa6fd7817481
arpitpardesi <arpit.pardesi6@gmail.com>
2020-07-29 13:55:21 +0530

    Pandas

--- Basics/Day4/Pandas/Ex3.py
+++ Basics/Day4/Pandas/Ex3.py
@@ -6,8 +6,10 @@ data1 = pd.DataFrame({"Name" : ['ABC','DEF'],
 
 data2 = pd.DataFrame({"Age"   : [22,25],
                       "Salary": [200000,400000]},
-                      index = [59128,59100])
+                      index = [59130,59100])
 
 df = data1.join(data2)
 print(df)
+df2 = data2.join(data1)
+print(df2)
 
