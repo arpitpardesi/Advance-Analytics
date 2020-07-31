908e45cf1663de326ceed0c4af694f702828587f
arpitpardesi <arpit.pardesi6@gmail.com>
2020-07-31 22:58:56 +0530

    Flask web app with sql lite

--- Basics/Day6/WebApp6/search4letters_web.py
+++ Basics/Day6/WebApp6/search4letters_web.py
@@ -6,17 +6,20 @@ from _sqlite3 import connect
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def open():
     return render_template('input.html')
 
-@app.route('/search', methods = ['post'])
+
+@app.route('/search', methods=['post'])
 def handle():
     p = request.form["phrase"]
     l = request.form["letters"]
     r = set(p).intersection(list(l))
     add_search(p, l, r)
-    return render_template('result.html', phrase = p, letter = l, result = r)
+    return render_template('result.html', phrase=p, letter=l, result=r)
+
 
 @app.route('/viewlog')
 def get_all():
@@ -24,12 +27,13 @@ def get_all():
     sql = """select * from records"""
     cursor = con.cursor()
     cursor.execute(sql)
-    a = cursor.fetchall()
-    return render_template('view_all.html', search_list = a)
+    x = cursor.fetchall()
+    return render_template('view_all.html', search_list=x)
 
-def add_search(phrase,letter,result):
+
+def add_search(phrase, letter, result):
     con = get_sqlite_connection()
-    sql = """insert into records values ("%s", "%s", "%s") """ %(phrase,letter,result)
+    sql = """insert into records values ("%s", "%s", "%s") """ % (phrase, letter, result)
     cursor = con.cursor()
     cursor.execute(sql)
     con.commit()
@@ -37,5 +41,6 @@ def add_search(phrase,letter,result):
 def get_sqlite_connection():
     con = connect('arpit_db.sql')
     return con
+
 
 app.run(port=2830)
